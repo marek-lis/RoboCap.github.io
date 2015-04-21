1316bd005c12d52774e24ea22018b12beba078f9
Zuzanna Pacholczyk <zuzannapacholczyk@wp.pl>
2015-04-21 16:51:02 +0200

    one page site

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,8 @@
 jQuery(document).ready(function($) {
-
-    /* ======= Scrollspy ======= */
-    $('body').scrollspy({ target: '#header', offset: 400});
-    
-    /* ======= Fixed header when scrolled ======= */
-    
-    $(window).bind('scroll', function() {
-		if ($(window).scrollTop() > 50) {
-             $('#header').addClass('navbar-fixed-top');
-         }
-         else {
-             $('#header').removeClass('navbar-fixed-top');
-         }
-    });
-    scrollTop: $(window.location.hash).offset().top 
-    /* ======= ScrollTo ======= */
-    $('a.scrollto').on('click', function(e){
-        
-        //store hash
-        var target = this.hash;
-                
-        e.preventDefault();
-		$('body').scrollTo(target, 800, {offset: -70, 'axis':'y', easing:'easeOutQuad'});
-        //Collapse mobile menu after clicking
-		if ($('.navbar-collapse').hasClass('in')){
-			$('.navbar-collapse').removeClass('in').addClass('collapse');
-		}
-		
+	
+	$('#fullpage').fullpage( {
+					css3: true,
+				scrollingSpeed: 1000
 	});
 
 });
