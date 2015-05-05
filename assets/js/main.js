5487f824bce5f5eb99fceac49ac414af161d70d7
Zuzanna Pacholczyk <zuzannapacholczyk@wp.pl>
2015-05-05 10:41:39 +0200

    fullpage working but menu not

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,16 @@
-jQuery(document).ready(function($) {
+$(document).ready(function() {
 
-	$('#fullpage').fullpage( {
-				responsive: 900
+	$('#fullpage').fullpage( {	
+	
+	sectionsColor: ['#17baef', '#f5f5f5', '#f5f5f5', '#17baef', '#f5f5f5', '#17baef'],
+				responsive: 900,
+				anchors: ['stronaGlowna', 'oNas', 'zespol', 'coRobimy', 'wydarzenia', 'kontakt'],
+				menu: '#menu',
+				scrollOverflow: true
+
 	});
- 
+
     /* ======= Fixed header when scrolled ======= */
     $('#header').addClass('navbar-fixed-top');
+
 });
